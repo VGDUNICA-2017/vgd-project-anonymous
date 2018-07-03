bbc20a517c9a3bf3187e7ce3357118111b80e3ce
Francesco Corrias <francesco.corrias.96@hotmail.it>
2018-07-03 16:09:35 +0200

    Added character with animations
    -Added character.
    -Added animation: walk, run, idle, jump.
    -Added third person camera.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+    public Transform cameraOrbit;
+    public Transform target;
+    public float cameraSpeed=5;
+
+    void Start() {
+        cameraOrbit.position = target.position;
+    }
+
+    void Update() {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0) {
+            cameraOrbit.transform.localScale = cameraOrbit.transform.localScale * (1f - scroll);
+        }
+
+        float horizontal = cameraSpeed * Input.GetAxis("Mouse X");
+        float vertical = cameraSpeed * Input.GetAxis("Mouse Y");
+
+        target.rotation = Quaternion.Euler(0, horizontal, 0);
+
+        if (cameraOrbit.transform.eulerAngles.z + vertical <= 0.1f || cameraOrbit.transform.eulerAngles.z + vertical >= 89.9f) {
+            vertical = 0;
+        }
+
+        cameraOrbit.transform.eulerAngles = new Vector3(cameraOrbit.transform.eulerAngles.x, cameraOrbit.transform.eulerAngles.y + horizontal, cameraOrbit.transform.eulerAngles.z + vertical);
+
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0);
+        transform.LookAt(target.position);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public bool isInputActive = true;
+    private Animator animator;
+    
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        if (isInputActive && (x != 0 || z != 0)) {
+            animator.SetBool("Moving", true);
+            if (isInputActive && Input.GetButtonDown("Fire3")) {
+                animator.SetBool("Running", true);
+            }
+            if (Input.GetButtonUp("Fire3")){
+                animator.SetBool("Running", false);
+            }
+            animator.SetFloat("Ahead", z);
+            animator.SetFloat("Direction", x);
+        }else {
+            animator.SetBool("Moving", false);
+            animator.SetBool("Running", false);
+        }
+
+        if (isInputActive && Input.GetButtonDown("Jump")) {
+            animator.SetBool("Jump", true);
+        }
+        if (Input.GetButtonUp("Jump")) {
+            animator.SetBool("Jump", false);
+        }
+        if (isInputActive && Input.GetButtonDown("Interact")) {
+            animator.SetBool("Pizza", true);
+        }
+        if (Input.GetButtonUp("Interact")) {
+            animator.SetBool("Pizza", false);
+        }
+
+    }
+}
